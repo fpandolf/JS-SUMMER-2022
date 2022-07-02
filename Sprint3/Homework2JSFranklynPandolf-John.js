62b79accbc00b1ec35b8d1adc6f18914e470056c
Frank <35009740+fpandolf@users.noreply.github.com>
2022-07-01 23:02:47 -0400

    after class 3 and HW2

---
+++ Sprint3/Homework2JSFranklynPandolf-John.js
@@ -0,0 +1,70 @@
+// Due Date : Tuesday Jul-05
+//Q1
+const sentence1 = 'Hello dear, how are you doing?';
+let result1 = 0;
+/**
+ * if the length of sentence1 is greater than or equals to 10
+ *      assign 15 in result1
+ * otherwise
+ *      assign 25 in result1
+ */
+//let sentence1Length = sentence1.length;       // no need to store
+//result1 = sentence1length >= 10 ? 15 : 25;    // no need to store
+result1 = sentence1.length >= 10 ? 15 : 25;
+
+console.log(`result1 is ${result1}`);
+console.log(`\n\n`);
+
+//Q2
+const sentence2 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
+/**
+ * replace all instances of a/A with 'Alpha', print the result in console
+ */
+ let sentence2Replace_a_Alpha = sentence2.replace(/a/gi, 'Alpha');
+ //console.log(`Reaplacing all instances of "a/A" with "Alpha" in sentence2 yields: ${sentence2Replace_a_Alpha}`);   // redone strictly following requirements
+ console.log(sentence2Replace_a_Alpha);
+ console.log(`\n\n`);
+
+//Q3
+const sentence3 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
+/**
+ * print the result in console:
+ * 1. the length of sentence-3
+ * 2. does sentence-3 starts with 'health' (ignore cases)
+ * 3. does sentence-3 contains with 'Body' (ignore cases)
+ * 4. index of 'Body' in sentence3  (ignore cases)
+ * 5. the last-character in sentence-3
+ * 6. word 'Body' is present only once. (true or false)
+ */
+//1
+let sentence3Length = sentence3.length;
+console.log(`The length of sentence3 is ${sentence3Length}`);
+//2
+let startsWithPattern1 = 'health';
+//let sentence3Lowercase = sentence3.toLowerCase();
+//let sentence3StartsWith_health = sentence3Lowercase.startsWith(startsWithPattern1);
+//console.log(`Does ${sentence3} start with 'health' (ignoring case) : ${sentence3StartsWith_health}`);
+//  below is the cleaner way
+let sentence3StartsWith_SWP1 = sentence3.toLowerCase().startsWith(startsWithPattern1.toLowerCase());
+console.log(`Does sentence3 start with "health" (ignoring case) : ${sentence3StartsWith_SWP1}`);
+//3
+let includesPattern1 = 'Body';
+let sentence3Includes_IP1 = sentence3.toLowerCase().includes(includesPattern1.toLowerCase());
+console.log(`Does sentence3 include "Body" (ignoring case) : ${sentence3Includes_IP1}`);
+//4
+let indexPattern1 = 'Body';
+let indexPosition_IP1 = sentence3.toLowerCase().indexOf(indexPattern1.toLowerCase());
+console.log(`"Body" first appears in sentence3 at index : ${indexPosition_IP1}`);
+//5
+//let sentence3Length = sentence3.length;                          // defined in first question
+//let indexPositionLastCharacterSentence3 = sentence3Length - 1;   // no need to store this value
+let lastCharacterSentence3 = sentence3.charAt((sentence3Length-1));
+console.log(`The last character in sentence3 is "${lastCharacterSentence3}" `);
+//6
+indexPattern2 = 'Body';
+let indexPosition_IP2 = sentence3.toLowerCase().indexOf(indexPattern2.toLowerCase());
+let lastIndexPattern2 = 'Body';
+let lastIndexPosition_IP2 = sentence3.toLowerCase().lastIndexOf(indexPattern2.toLowerCase());
+//console.log(`Body last appears in sentence3 at index : ${lastIndexPosition_IP1}`);
+let isIP2EqLIP2 = (indexPosition_IP2 === lastIndexPosition_IP2) && (indexPosition_IP2 >= 0);   // the two index values must be equal and not be -1 (invalid) to show indexPattern2 occurs just once
+console.log(`Is "Body" present just once in sentence3: ${isIP2EqLIP2} `);
--- Sprint3/StringsConcept.js
+++ Sprint3/StringsConcept.js
@@ -6,7 +6,7 @@ Length
     property of string class to get numer of characters in string object.
 
 **/
-let cityName = 'New YOrk ciTy'
+//let cityName = 'New YOrk ciTy'
 // let cityNameLength = cityName.length
 // console.log(`Length = ${cityNameLength}`);
 
@@ -18,10 +18,10 @@ Methods  //  Functions
         does not change original value in variable
  */
 
-let cityName_UpperCase = cityName.toUpperCase()
-console.log(`cityName is ${cityName}`);
-console.log(`cityName_UpperCase is ${cityName_UpperCase}`);
-console.log(`\n`);
+// let cityName_UpperCase = cityName.toUpperCase()
+// console.log(`cityName is ${cityName}`);
+// console.log(`cityName_UpperCase is ${cityName_UpperCase}`);
+// console.log(`\n`);
 /**
 Methods  //  Functions
 
@@ -30,10 +30,10 @@ Methods  //  Functions
         does not change original value in variable
  */
 
-let cityName_LowerCase = cityName.toLowerCase()
-console.log(`cityName is ${cityName}`);
-console.log(`cityName_LowerCase is ${cityName_LowerCase}`);
-console.log(`\n`);
+// let cityName_LowerCase = cityName.toLowerCase()
+// console.log(`cityName is ${cityName}`);
+// console.log(`cityName_LowerCase is ${cityName_LowerCase}`);
+// console.log(`\n`);
 
 /**
 Methods  //  Functions
@@ -43,19 +43,161 @@ Methods  //  Functions
         does not change original value in variable
  */
 
-        let cityName_StartsWith_n = cityName.startsWith('n')
-console.log(`cityName is ${cityName}`);
-console.log(`cityName_StartsWith_n is ${cityName_StartsWith_n}`);
-console.log(`\n`);
+//         let cityName_StartsWith_n = cityName.startsWith('n')
+// console.log(`cityName is ${cityName}`);
+// console.log(`cityName_StartsWith_n is ${cityName_StartsWith_n}`);
+// console.log(`\n`);
 
-let cityName_StartsWith_New_Y = cityName.startsWith('New Y')
-console.log(`cityName is ${cityName}`);
-console.log(`cityName_StartsWith_New_Y is ${cityName_StartsWith_New_Y}`);
-console.log(`\n`);
+// let cityName_StartsWith_New_Y = cityName.startsWith('New Y')
+// console.log(`cityName is ${cityName}`);
+// console.log(`cityName_StartsWith_New_Y is ${cityName_StartsWith_New_Y}`);
+// console.log(`\n`);
 
 // Combined to make input not need to be case sensitive
 
-let startsWithPattern = "neW Y"
-startsWithPattern = startsWithPattern.toLocaleUpperCase()    // NEW Y
-cityName_UpperCase = cityName.toUpperCase()                  // NEW YORK CITY
-let cityName_StartsWith_neW_Y = cityName_UpperCase.startsWith(startsWithPattern)
+// let startsWithPattern = "neW Y"
+// startsWithPattern = startsWithPattern.toLocaleUpperCase()    // NEW Y
+// cityName_UpperCase = cityName.toUpperCase()                  // NEW YORK CITY
+// let cityName_StartsWith_neW_Y = cityName_UpperCase.startsWith(startsWithPattern)
+/**
+ * **********************************From DEEPAK "***************************
+/**
+ * Example for ignoring pattern with StartsWith
+ */
+
+// let startsWithPattern = 'neW y';
+// startsWithPattern = startsWithPattern.toUpperCase();    // NEW Y 
+// let cityName_Uppercase = cityName.toUpperCase();        // NEW YORK CITY
+// let cityNameStartsWith_neW_Y = cityName_Uppercase.startsWith(startsWithPattern);
+// console.log(`\ncityName -> ${cityName}`);
+// console.log(`is ${cityName} start with 'neW Y' : ${cityNameStartsWith_neW_Y}`);
+
+// let res = cityName.toUpperCase().startsWith(startsWithPattern.toUpperCase());
+// console.log(res);
+// ******************************************************************************************************************
+
+
+
+//Methods  //  Functions
+
+//let cityName = 'New YOrK ciTy'
+// Ends with
+// endsWith()  - checks if string ends with exact given pattern
+// returns boolean
+
+// let cityNameEndsWith_K_ciTy = cityName.endsWith('K ciTy');
+// console.log(`cityNameEndsWith_K_ciTy is ${cityNameEndsWith_K_ciTy}`);
+
+// let cityNameIncludes_rK_ci = cityName.includes('rK ci');
+// console.log(`cityNameIncludes_rK_ci is ${cityNameIncludes_rK_ci}`);
+
+// let abc 
+// console.log(abc);
+
+
+/**
+ * startsWith(), endsWith(), and includes()
+ * input # = 1
+ * input type = string
+ * result type = boolean
+ */
+
+
+
+// Function 
+// Replace
+// replace a given pattern from string with another pattern/value
+// replace()
+//input # = 2
+// input type = 2 strings ( item replaced, and item to replace with)
+// return type = string
+
+// let sentence = 'I have a cAt'
+// let sentenceReplace_a_Z = sentence.replace('a', 'Z')
+// console.log(`sentenceReplace_a_Z is ${sentenceReplace_a_Z}`);
+
+// by default only the first instance
+
+// (/a/g, 'Z') replaces all lower case a to Z   //            /g = global
+// (/a/gi, 'Z') replaces all a to Z regardles of case  //     /i = ignore case
+
+
+/**
+ * Function to join 2 or more strings
+ * 
+ * concat()
+ * str1.concat(str2,str3,str4)
+ * or
+ * str1 + str2 + str3 + str4
+ */
+
+
+
+// Funtion Trim
+//  trim()
+// removes spaces before and after genuine characters (does not work between genuine characters 'H' and '!')
+// '    Hi    !      '   becomes 'Hi    !'
+// let sent = '      a    b       c     '
+// console.log(`"${sent.trim()}"`);
+
+//Function Pad
+//padStart()
+//adds padding at the beginning 
+//padEND()
+//adds padding at the end
+//input# = 2
+//input type =
+//  number : length of string after padding
+//  String : character we would like to use for padding
+// result type =
+//  string: string after padding
+// Hello padStart(7,*) = **Hello
+// Hello padStart(4,*) = Hello  // Hello was already 4 or more characters
+
+// let pString = 'Hello';
+// let pStringAfterPadding7 = pString.padStart(7,'*');
+// console.log(`pStringAfterPadding7 is ${pStringAfterPadding7}`);
+// let pStringAfterPadding4 = pString.padStart(4,'*');
+// console.log(`pStringAfterPadding4 is ${pStringAfterPadding4}`);
+// let pStringAfterPaddingE12 = pString.padEnd(12,'abc');
+// console.log(`pStringAfterPaddingE12 is ${pStringAfterPaddingE12}`);
+// let pStringAfterPaddingE4 = pString.padEnd(4,'abc');
+// console.log(`pStringAfterPaddingE4 is ${pStringAfterPaddingE4}`);
+
+
+ /**
+  * Function  charAT
+  * finds character at given index
+  * input# = 1
+  * input type = number (location in index to check what character is there) 
+  * 
+  */
+ let sentence = 'asdfasdfasdfasdf'
+//  console.log(sentence.charAt(2));
+//  console.log(sentence.charAt(200));   //beyond the scope (invalid in this case) returns nothing
+//  console.log(sentence.charAt(-4));   // also invalid  (always for negative value) returns nothing
+
+ /**
+  * Function indexOf()
+  * to find at which index the given pattern is exactly present in the string  (first occurence)
+  * input # = 1
+  * input type = string
+  * result = number : index where the pattern first occurs or -1 if not present
+  */
+//  console.log(sentence.indexOf('a'));   // 0  first occurence @ index 0 
+//  console.log(sentence.indexOf('b'));    // -1 invalid vecause does not exist in the string
+//  console.log(sentence.indexOf('sdf'));  // 1   first occurence @ index 1  whole pattern starts from there
+//  console.log(sentence.indexOf('A'));    // -1 (case sensitive)
+
+
+ /**
+  * Function lastIndexOf()
+  * to find the index at which last occurence of pattern
+  *  input # = 1
+  * input type = string
+  * result = number : index where the pattern last occurs  or -1 if not present
+  */
+
+  console.log(sentence.lastIndexOf('a'))
+  console.log(sentence.lastIndexOf('sdf'))
+  console.log(sentence.lastIndexOf('b'))
